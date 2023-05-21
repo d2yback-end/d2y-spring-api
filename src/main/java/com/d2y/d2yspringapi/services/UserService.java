c84ee93e4c8b05f4feb810b4d46adc7a92f9195e
AdiMunawar31 <munawar.adi31@gmail.com>
2023-05-22 06:13:58 +0700

    add some change at user service

--- src/main/java/com/d2y/d2yspringapi/services/UserService.java
+++ src/main/java/com/d2y/d2yspringapi/services/UserService.java
@@ -1,5 +1,56 @@
 package com.d2y.d2yspringapi.services;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.d2y.d2yspringapi.exceptions.UserNotFoundException;
+import com.d2y.d2yspringapi.interfaces.UserServiceInterface;
+import com.d2y.d2yspringapi.models.Role;
+import com.d2y.d2yspringapi.models.User;
+import com.d2y.d2yspringapi.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserServiceInterface {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  @Override
+  public User getUserById(Integer id) {
+    User user = findUserById(id);
+    return user;
+  }
+
+  @Override
+  public void updateUserRole(Integer id, Role newRole) {
+    User user = findUserById(id);
+
+    user.setRole(newRole);
+    userRepository.save(user);
+  }
+
+  @Override
+  public void deleteUser(Integer id) {
+    findUserById(id);
+    userRepository.deleteById(id);
+  }
+
+  private User findUserById(Integer id) {
+    Optional<User> userExists = userRepository.findById(id);
+    if (userExists.isPresent()) {
+      return userExists.get();
+    } else {
+      throw new UserNotFoundException("User not found with id: " + id);
+    }
+  }
 
 }
