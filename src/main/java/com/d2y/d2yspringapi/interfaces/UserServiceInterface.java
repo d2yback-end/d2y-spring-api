4a911993518a0a909e4e424208be371af9469e00
AdiMunawar31 <munawar.adi31@gmail.com>
2023-05-21 20:08:03 +0700

    modify user service interface

--- src/main/java/com/d2y/d2yspringapi/interfaces/UserServiceInterface.java
+++ src/main/java/com/d2y/d2yspringapi/interfaces/UserServiceInterface.java
@@ -1,10 +1,18 @@
 package com.d2y.d2yspringapi.interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.d2y.d2yspringapi.models.User;
 
 public interface UserServiceInterface {
-    List<User> getUsers();
+
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    User updateUser(User user);
+
+    void deleteUser(Long id);
 
 }
